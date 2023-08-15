685ae6e3e5c1bef130865df80cfe0c9a26d0499c
nominalrune <59037577+nominalrune@users.noreply.github.com>
2023-08-15 14:17:51 +0900

    deleted needless calcs

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use rodio::{source::Source, Decoder, OutputStream};
 use std::{
     fs::File,
     time::Duration,
-    cmp::Ordering,
     thread::{sleep, spawn},
     io::{BufReader, stderr, Write},
 };
@@ -26,42 +25,18 @@ fn play(_file: &str) {
 
 fn count_down(seconds: i32, callback: fn()) {
     let interval = Duration::from_secs(1);
-    let mut _hours = seconds / 3600;
-    let mut _minutes = (seconds % 3600) / 60;
-    let mut _seconds = seconds % 60;
+    let mut _seconds = seconds;
     let mut stream = stderr();
     loop {
-        match _seconds.cmp(&0) {
-            Ordering::Less => {
-                _minutes -= 1;
-                _seconds = 59;
-                if _hours > 0 && _minutes <= 0 {
-                    _hours -= 1;
-                    _minutes = 59;
-                }
-            }
-            Ordering::Equal => {
-                _minutes -= 1;
-                _seconds = 59;
-                if _hours > 0 && _minutes <= 0 {
-                    _hours -= 1;
-                    _minutes = 59;
-                }
-            }
-            Ordering::Greater => {
-                _seconds -= 1;
-            }
-        }
-        if _hours == 0 && _minutes == 0 && _seconds == 0 {
+        _seconds -= 1;
+        if _seconds <= 0 {
             spawn(move || {
                 callback();
             });
-            _hours = seconds / 3600;
-            _minutes = seconds / 60;
-            _seconds = seconds % 60;
+            _seconds = seconds;
         }
         stream
-            .write(format!("\r{:02}:{:02}:{:02}", _hours, _minutes, _seconds).as_bytes())
+            .write(format!("\rnext: {:02}:{:02}:{:02}", _seconds / 3600, _seconds / 60 % 60, _seconds % 60).as_bytes())
             .unwrap();
         stream.flush().unwrap();
         sleep(interval);
